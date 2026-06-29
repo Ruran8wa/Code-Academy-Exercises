70b917f9e5f03af676b551e6ffa2033caa4d95cd
ruran8wa <p.rurangwa@alustudent.com>
2026-06-29 10:59:22 +0200

    variables and data types

---
+++ Code-Academy-Exercises/Data-Types/Variable-Types.cs
@@ -0,0 +1,11 @@
+namespace Code_Academy_Exercises.Data_Types;
+
+public class Variable_Types
+{
+    static void Main(string[] args)
+    {
+        string name = "Shadow";
+        string breed = "Golden Retriever";
+        Console.WriteLine($"The dog's name is {name}, and it's breed is {breed}");
+    }
+}
