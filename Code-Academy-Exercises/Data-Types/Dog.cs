2e6831d57113c39bdb45ca2bb0318c9938f7fe1e
ruran8wa <p.rurangwa@alustudent.com>
2026-06-29 11:15:55 +0200

    replaced the variable types with var

--- Code-Academy-Exercises/Data-Types/Dog.cs
+++ Code-Academy-Exercises/Data-Types/Dog.cs
@@ -1,15 +1,15 @@
 namespace Code_Academy_Exercises.Data_Types;
 
-public class VariableTypes
+public class Dog
 {
     static void Main(string[] args)
     {
         // Create Variables
-        string name = "Shadow";
-        string breed = "Golden Retriever";
-        int age = 5;
-        double weight = 65.22;
-        bool spayed = true;
+        var name = "Shadow";
+        var breed = "Golden Retriever";
+        var age = 5;
+        var weight = 65.22;
+        var spayed = true;
 
         // Print variables to the console
         Console.WriteLine(name);
